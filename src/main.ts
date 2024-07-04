0ecdf84936a926e2a57b35f83d6006490c5a7aa0
Akinori Nakata <akinori.nakata@bright-century.com>
2024-07-04 16:13:29 +0900

    URL Prefix 設定

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import configuration from './config/configuration';
 async function bootstrap() {
   const options = { cors: configuration().app.cors };
   const app = await NestFactory.create(AppModule, options);
+  app.setGlobalPrefix('/api');
   await app.listen(configuration().app.port);
 }
 bootstrap();
